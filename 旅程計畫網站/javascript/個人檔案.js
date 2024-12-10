2b7e5ae94d3d00cb61bfd25c070d5d717bdaaa2d
developer-Jimmy <g12347825@gmail.com>
2024-12-10 11:26:57 +0800

    早上修改

--- 旅程計畫網站/javascript/個人檔案.js
+++ 旅程計畫網站/javascript/個人檔案.js
@@ -61,7 +61,7 @@ function renderPlans(plans, container, statusClass, btnClass) {
       <a class="btn btn-secondary">相關照片</a>
       <a class="btn btn-secondary">相關影片</a>
       <button class="btn ${btnClass}" onclick="viewDetails(${index})">查看詳細</button>
-      <a class="btn btn-info">分享計畫</a>
+      <a class="btn btn-info">共享計畫</a>
       <a class="btn btn-danger">刪除計畫</a>
     `;
     container.appendChild(planCard);
